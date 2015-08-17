84bbbf82dfb4f700963d1ae253d26296467d3327
Benedikt <Benedikt@Benedikt-PC.fritz.box>
2015-08-17 23:24:45 +0200

    added onEnable

--- src/me/benfah/main/MainClass.java
+++ src/me/benfah/main/MainClass.java
@@ -3,5 +3,23 @@ package me.benfah.main;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainClass extends JavaPlugin{
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void onEnable()
+	{
+		
+		
+		
+		
+		
+		System.out.println("Hi!");
+		
+	}
 }
